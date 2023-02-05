8805ec6a3517e487d8b04dc2d947f5d917344997
kolas <kolas.batman@gmail.com>
2023-02-05 15:41:09 +0300

    initial commit

---
+++ lib/grpc_kit_server.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require_relative "grpc_kit_server/version"
+require_relative "grpc_kit_server/rpc_server"
+
+module GrpcKitServer
+  # class Error < StandardError; end
+
+  # def self.logger
+  #   @logger ||= Logger.new(STDOUT)
+  # end
+
+  # def self.logger=(log)
+  #   @logger = log
+  # end
+end
